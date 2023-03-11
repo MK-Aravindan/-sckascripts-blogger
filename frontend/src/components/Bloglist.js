fbd82bdc5436e8da2b95615e9bf7a94aa98c78f6
MK-Aravindan <aravindanmk02@gmail.com>
2023-03-11 20:52:30 +0530

    Added Search box

--- frontend/src/components/Bloglist.js
+++ frontend/src/components/Bloglist.js
@@ -1,16 +1,38 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Bloglist = ({ blogs }) => {
+  const [search, setSearch] = useState("");
+
   return (
     <div className="blog-list">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <Link to={`/blogs/${blog.id}`}>
-            <h2>{blog.title}</h2>
-            <p>Written by {blog.author}</p>
-          </Link>
-        </div>
-      ))}
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>{" "}
+      <div className="search-box-line"></div>
+      {blogs
+        .filter((blog) => {
+          const title = blog.title.toLowerCase();
+          const author = blog.author.toLowerCase();
+          const searchString = search.toLowerCase();
+
+          return (
+            title.includes(searchString) ||
+            author.includes(searchString)
+          );
+        })
+        .map((blog) => (
+          <div className="blog-preview" key={blog.id}>
+            <Link to={`/blogs/${blog.id}`}>
+              <h2>{blog.title}</h2>
+              <p>Written by {blog.author}</p>
+            </Link>
+          </div>
+        ))}
     </div>
   );
 };
